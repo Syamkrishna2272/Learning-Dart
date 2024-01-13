24cd49c63501df8154e5e71293144c038fbc50c0
Syamkrishna2272 <syamkrishnashyam@gmail.com>
2024-01-13 05:49:30 +0530

    query added

---
+++ bin/sql_queries.dart
@@ -0,0 +1,17 @@
+// //DB create
+
+// _db=await openDatabase('abc.db',version:1,oncreate(Database db,int version){
+//   db.execute('CREATE TEBLE abc (id INTEGER PRIMARY KEY,NAME TEXT,age TEXT)');
+// });
+
+// //GET db
+
+// Future<void>getallabc()async{
+//   _db.rawquery('SELECT * FROM abc');
+// }
+
+// //INSERT
+
+// Future<void>addabc(abcmodel value)async{
+//   await _db.rawInsert('INSERT INTO abc (name,age)VALUES(?,?)[value.name,value.age]');
+// }
